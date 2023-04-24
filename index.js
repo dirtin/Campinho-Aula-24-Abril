173583038bd045243aa6bd8a233445c64384a2c2
Julli Mayanne <jullimayanne9@gmail.com>
2023-04-24 12:34:19 -0300

    feat: start

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+Declara uma variável "i" e atribui o valor 0 a ela
+Declara uma variável "j" sem atribuir valor
+Atribui o valor 10 à variável "i"
+Atribui a string "Mundo" à variável "j"
+*/
+
+/*
+ Declara uma variável "x" e atribui o valor 5 a ela
+ Declara uma variável "y" sem atribuir valor
+ Atribui o valor 10 à variável "x"
+ Atribui a string "Olá" à variável "y"
+*/
+
+/*
+Desafio
+
+Declara uma constante com valor 3.14
+Declara uma constante com valor "João"
+Declara uma constante que armazena um array
+Gera um erro, pois não é possível reatribuir valor a uma constante
+
+*/
