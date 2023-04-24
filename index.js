291ba3d6c785e8e623b0242558e5331d4a3ab41c
Julli Mayanne <jullimayanne9@gmail.com>
2023-04-24 13:17:17 -0300

    feat: add new questions

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ Exibir para o cliente o custo total da conta juntamente com a entrega.
 /*
 
 
-2 - Levando em consideração que existem dois tipos de juros aplicados no mercado fincanceiro e que o mais utilizado é o composto elabore um programa que ajude uma pessoa a saber quanto irá gastar em caso de empréstimo bancário no valor de  R$ 15.000,00 que será  pago em 24 parcelas mensais com juros compostos de 1,5% ao mês. 
+2 - Levando em consideração que existem dois tipos de juros aplicados no mercado 
+fincanceiro e que o mais utilizado é o composto. Elabore um programa que ajude uma
+pessoa a saber quanto irá gastar em caso de empréstimo bancário no valor de 
+R$ 15.000,00 que será  pago em 24 parcelas mensais com juros compostos 
+de 1,5% ao mês. Você deve exibir o valor de cada parcela, 
+além do valor total do emprestimo.
 
 Entrada de dado: 
 Valor inicial do emprestimo: 15.000 R$
@@ -30,7 +35,6 @@ Processamento :
 Calcular o juros usando a formula para juros compostos  V = P (1 + r/n)^(nt), onde V é o valor final, P é o principal (ou valor inicial), r é a taxa de juros mensal, n é o número de vezes que os juros são compostos por mês e t é o tempo em meses. 
 
 Saída:
-Exibir para o cliente o valor final do emprestimo 
-
+Exibir para o cliente o valor final do emprestimo e o de cada parcela
 
 */
