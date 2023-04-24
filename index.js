8d324dd631d91d8e444ff6eb9ed29edaecc0d9c4
Julli Mayanne <jullimayanne9@gmail.com>
2023-04-24 13:14:40 -0300

    fix: gitignore and index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+/*
 
+1 - Elabore um programa que ajude um restaurante a calcular o valor final
+ de um pedido para os seus clientes. A conta é calculada junto com o valor 
+ da refeição e a taxa de entrega. 
+
+Entrada de dado: 
+O valor da conta de 150 R$
+
+Processamento :
+Calcular a conta junto com o valor da entrega entrega para o endereço do cliente: 18 R$
+
+Saída: 
+Exibir para o cliente o custo total da conta juntamente com a entrega. 
+
+
+
+*/
+
+
+/*
+
+
+2 - Levando em consideração que existem dois tipos de juros aplicados no mercado fincanceiro e que o mais utilizado é o composto elabore um programa que ajude uma pessoa a saber quanto irá gastar em caso de empréstimo bancário no valor de  R$ 15.000,00 que será  pago em 24 parcelas mensais com juros compostos de 1,5% ao mês. 
+
+Entrada de dado: 
+Valor inicial do emprestimo: 15.000 R$
+
+Processamento :
+Calcular o juros usando a formula para juros compostos  V = P (1 + r/n)^(nt), onde V é o valor final, P é o principal (ou valor inicial), r é a taxa de juros mensal, n é o número de vezes que os juros são compostos por mês e t é o tempo em meses. 
+
+Saída:
+Exibir para o cliente o valor final do emprestimo 
+
+
+*/
